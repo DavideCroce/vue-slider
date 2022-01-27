eec3e870b34e730396d109d5ebb0b112564b0920
IskahnX <davidecroce10@gmail.com>
2022-01-27 15:34:51 +0100

    image added also in the data

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log('VUE OK!', Vue);
+
+const TrueDamage = new Vue ({
+    el: '#root',
+    data: {
+        team: [
+            {ekko: 'img/ekko.jpg'},
+            {senna: 'img/senna.jpg'},
+            {akali: 'img/akali.jpg'},
+            {yasuo: 'img/yasuo.jpg'},
+            {qiyana: 'img/qiyana.jpg'}
+        ]
+    },
+    methods:{
+        
+    }
+})
