aba136ed39f085749f16421144344ab1dbdeb7d0
IskahnX <davidecroce10@gmail.com>
2022-01-29 13:01:10 +0100

    slider done (also functions for the slide)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 console.log('VUE OK!', Vue);
 
+Vue.config.devtools = true;
+
 const TrueDamage = new Vue ({
     el: '#root',
     data: {
+        currentIndex: 0,
         team: [
-            {ekko: 'img/ekko.jpg'},
-            {senna: 'img/senna.jpg'},
-            {akali: 'img/akali.jpg'},
-            {yasuo: 'img/yasuo.jpg'},
-            {qiyana: 'img/qiyana.jpg'}
-        ]
+            'img/ekko.jpg',
+            'img/senna.jpg',
+            'img/akali.jpg',
+            'img/yasuo.jpg',
+            'img/qiyana.jpg',
+        ],
+        names: [
+            'EKKO',
+            'SENNA',
+            'AKALI',
+            'YASUO',
+            'QIYANA',
+        ],
     },
     methods:{
-        
+        active(index){
+            return index === this.currentIndex;
+        },
+        setCharacter(index){
+            this.currentIndex = index;
+        },
+        before(){
+            this.currentIndex = this.currentIndex - 1;
+        },
+        after(){
+            this.currentIndex = this.currentIndex + 1;
+        }
     }
 })
